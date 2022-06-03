5d2d5f88e48d637a2f4c3ae4c23a7d7f956b1efd
Jhonattan Vinicios De Oliveira Gome <jhonattangome@sisconsultoria.com.br>
2022-06-03 15:55:36 -0300

    Hello Word Node.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello Word!"); // console.log no javascript printa algo na tela
+console.log("Meu nome é Jhonattan");
+console.log("E eu estou aprendendo Node.js com o Guia do Programador");
+
+if(mostrarSite){ // Se mostrar site
+    console.log(site); // Printe o site
+}
