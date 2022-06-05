cd658d3ad655157814c0ba3ae9db99b2c69e0e06
Jhonattan Vinicios De Oliveira Gome <jhonattangome@sisconsultoria.com.br>
2022-06-05 11:18:34 -0300

    Alteração de teste

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ console.log("Hello Word!"); // console.log no javascript printa algo na tela
 console.log("Meu nome é Jhonattan");
 console.log("E eu estou aprendendo Node.js com o Guia do Programador");
 
-if(mostrarSite){ // Se mostrar site
+if(mostrarSite) { // Se mostrar site
     console.log(site); // Printe o site
 }
